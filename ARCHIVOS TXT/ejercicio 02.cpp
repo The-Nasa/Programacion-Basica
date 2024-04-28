6c02481ce4adf4b1bb08ace0909ac8f3e8fd1717
Brennis Castro <castrocanobrennis@gmail.com>
2024-04-28 15:12:07 -0500

    Fix_access_txt

--- ARCHIVOS TXT/ejercicio 02.cpp
+++ ARCHIVOS TXT/ejercicio 02.cpp
@@ -1,17 +1,31 @@
 #include <fstream>
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::ofstream archivo("ejemplo.txt"); // Crea o abre el archivo "ejemplo.txt" para escritura
-    
-    if (archivo.is_open()) {
-        archivo << "Hola, mundo!\n"; // Escribe en el archivo
+string ingresarNombre()
+{
+    string nombre;
+
+    cout << "Ingrese su nombre: " << endl;
+
+    cin >> nombre;
+
+    return nombre;
+}
+int main()
+{
+    std::ofstream archivo("ejemplo.txt", ios::app); // Crea o abre el archivo "ejemplo.txt" para escritura
+
+    if (archivo.is_open())
+    {
+        archivo << ingresarNombre() << endl; // Escribe en el archivo
         archivo << "Estoy aprendiendo C++.\n";
         archivo.close(); // Cierra el archivo
-    } else {
+    }
+    else
+    {
         std::cout << "No se pudo abrir el archivo para escritura.\n";
     }
 
     return 0;
 }
-
